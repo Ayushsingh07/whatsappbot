c8c54883a15b1a1705d60a5db87d1c590b7c6798
Ayushsingh07 <nqu7069@gmail.com>
2022-10-31 23:23:39 +0530

    more

--- bot.js
+++ bot.js
@@ -15,12 +15,17 @@ client.on('qr', qr => {
 
 client.on('message',async message => {
     const content = message.body
-    if (content=="pls meme"){
+    if (content=="meme"){
         const meme = await axios("https://meme-api.herokuapp.com/gimme").then(res =>res.data)
         
         client.sendMessage(message.from, await MessageMedia.fromUrl(meme.url))
     }
-
+    else if (content=="joke"){
+        const joke = await axios("https://v2.jokeapi.dev/joke/Any?safe-mode").then(res =>res.data)
+        
+        const jokemsg =await client.sendMessage(message.from,joke.setup)
+        if(joke.delivery) setTimeout(function(){jokemsg.reply(joke.delivery)},500)
+    }
     
 });
 
