10ff2b252c2bb5668cfcad527dd826652576e5c8
Ayushsingh07 <nqu7069@gmail.com>
2023-02-20 14:51:47 +0530

    modified:   bot.js

--- bot.js
+++ bot.js
@@ -19,21 +19,22 @@ app.get('/client', (req, res) => {
     res.sendStatus(200);
 });
 
-// client.on('message',async message => {
-//     const content = message.body
-//     if (content=="meme"){
-//         //const meme = await axios("https://meme-api.herokuapp.com/gimme").then(res =>res.data)
+
+client.on('message',async message => {
+    const content = message.body
+    if (content==""){
+        const meme = await axios("https://meme-api.herokuapp.com/gimme").then(res =>res.data)
         
-//         client.sendMessage(message.from, await MessageMedia.fromUrl(meme.url))
-//     }
-//     else if (content=="joke"){
-//         const joke = await axios("https://v2.jokeapi.dev/joke/Any?safe-mode").then(res =>res.data)
+        client.sendMessage(message.from, await MessageMedia.fromUrl(meme.url))
+    }
+    else if (content=="joke"){
+        const joke = await axios("https://v2.jokeapi.dev/joke/dark").then(res =>res.data)
         
-//         const jokemsg =await client.sendMessage(message.from,joke.setup)
-//         if(joke.delivery) setTimeout(function(){jokemsg.reply(joke.delivery)},500)
-//     }
+        const jokemsg =await client.sendMessage(message.from,joke.setup)
+        if(joke.delivery) setTimeout(function(){jokemsg.reply(joke.delivery)},500)
+    }
     
-// });
+ });
 
 
-// client.initialize();
+ client.initialize();
